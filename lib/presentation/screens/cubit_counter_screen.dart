a8573dbc26310d812d345fc9a531270ecf69137a
Diego Rios <riosdiego498@gmail.com>
2024-04-30 19:23:07 -0400

    first commit

---
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_cubit/counter_cubit.dart';
+
+class CubitCounterScreen extends StatelessWidget {
+  const CubitCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // double screenWidht = MediaQuery.of(context).size.width;
+    double screenHeidht = MediaQuery.of(context).size.height;
+    return BlocProvider(
+        create: (_) => CounterCubit(),
+        child: _CubirCounterView(screenHeidht: screenHeidht));
+  }
+}
+
+class _CubirCounterView extends StatelessWidget {
+  const _CubirCounterView({
+    required this.screenHeidht,
+  });
+
+  final double screenHeidht;
+
+  void increaseCounterBy(BuildContext context, [int value = 1]) {
+    context.read<CounterCubit>().increaseBy(value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final counterState = context.watch<CounterCubit>().state;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cubit Counter : ${counterState.transactionCount}'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                context.read<CounterCubit>().reset(0);
+              },
+              icon: const Icon(Icons.refresh_outlined)),
+        ],
+      ),
+      body: Center(
+        child: BlocBuilder<CounterCubit, CounterState>(
+          builder: (context, state) {
+            print('Counter cambió');
+            return Text('Counter value: ${state.counter}');
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            child: const Text('+3'),
+            onPressed: () {
+              increaseCounterBy(context, 3);
+            },
+          ),
+          SizedBox(
+            height: screenHeidht * 0.015,
+          ),
+          FloatingActionButton(
+            heroTag: '2',
+            child: const Text('+2'),
+            onPressed: () {
+              increaseCounterBy(context, 2);
+            },
+          ),
+          SizedBox(
+            height: screenHeidht * 0.015,
+          ),
+          FloatingActionButton(
+            heroTag: '3',
+            child: const Text('+1'),
+            onPressed: () {
+              increaseCounterBy(
+                context,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,4 @@
+export 'package:forms_app/presentation/screens/cubit_counter_screen.dart';
+
+export 'package:forms_app/config/theme/app_theme.dart';
+export 'package:forms_app/presentation/screens/home_screen.dart';
