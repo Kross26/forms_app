d1c2f0a5569836362edac94a3923de0323bf8373
Diego Rios <riosdiego498@gmail.com>
2024-05-02 18:40:58 -0400

    new route

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,4 +1,3 @@
-import 'package:forms_app/presentation/screens/bloc_counter_screen.dart';
 import 'package:forms_app/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
@@ -14,5 +13,9 @@ final appRouter = GoRouter(routes: [
   GoRoute(
     path: '/counter-bloc',
     builder: (context, state) => const BlocCounterScreen(),
+  ),
+  GoRoute(
+    path: '/new-user',
+    builder: (context, state) => const RegisterScreen(),
   ),
 ]);
